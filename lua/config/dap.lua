d742d6e5a7cc2c06844e86489e6bad632750942d
ironlungx <hwlooverhello@gmail.com>
2025-07-31 06:55:08 +0100

    dap config for c/c++

---
+++ lua/config/dap.lua
@@ -0,0 +1,60 @@
+local dap = require("dap")
+local ui = require("dapui")
+
+-- Setup dap-ui with default config
+ui.setup()
+
+-- Define the LLDB adapter
+dap.adapters.lldb = {
+	type = "executable",
+	command = "lldb-dap-18", -- change this to the path of your lldb-vscode executable
+	name = "lldb",
+}
+
+-- C++ debug configuration
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+		runInTerminal = false,
+	},
+}
+
+-- For convenience, use the same config for C
+dap.configurations.c = dap.configurations.cpp
+
+-- Optional: Open dap-ui automatically on debug start
+vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+-- Eval var under cursor
+vim.keymap.set("n", "<space>?", function()
+	require("dapui").eval(nil, { enter = true })
+end)
+
+vim.keymap.set("n", "<F1>", dap.continue)
+vim.keymap.set("n", "<F2>", dap.step_into)
+vim.keymap.set("n", "<F3>", dap.step_over)
+vim.keymap.set("n", "<F4>", dap.step_out)
+vim.keymap.set("n", "<F5>", dap.step_back)
+vim.keymap.set("n", "<F13>", dap.restart)
+
+dap.listeners.before.attach.dapui_config = function()
+	ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	ui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	ui.close()
+end
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			require("config.dap")
+		end,
+	},
+}
